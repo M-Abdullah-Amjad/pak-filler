27fae5b592485769be6aec8f62aafac7204185e2
Muhammad Abdullah Amjad <abdullahamjadlhr2020@gmail.com>
2025-08-10 19:24:16 +0500

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://ismailkhan194714:9sC0VWurP3YoIGQF@pakfiller1.wgl0knf.mongodb.net/pakfiller_db?retryWrites=true&w=majority&appName=Pakfiller1');
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error('MongoDB connection error:', error);
